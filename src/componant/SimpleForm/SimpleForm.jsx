4efd375b20f07571221d02604771c7917f497b63
me2sumanislam <me2sumanislam247@gmail.com>
2026-03-11 22:44:17 +0600

    form theke amra kono akta input ar name set kore tar valu ta ke console.log kore dekhachi module-16.1

--- src/componant/SimpleForm/SimpleForm.jsx
+++ src/componant/SimpleForm/SimpleForm.jsx
@@ -3,8 +3,12 @@
 //task-1 submit button click korle nicher function call hobe 
 
  function SimpleForm() {
-    const handleSubmit = () =>{
-        console.log('form submitted')
+    const handleSubmit = (e) =>{
+        // preventDefault use kore browser default behevior stop korte pari .dile amra console.log dekhte parbo.preventDefault na dile browser auto load nibe . 
+        e.preventDefault();
+        // 
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
     }
 
 
@@ -13,7 +17,9 @@
    return (
      <div>
        <form onSubmit={handleSubmit}>
-        <input type = "text"  placeholder='Your Name' />
+        <input  name="name" type = "text"  placeholder='Your Name' />
+        <br />
+        <input type="email" name="email" id="" placeholder='Your Email' />
         <br />
         <input type="submit" value="Submit" />
 
