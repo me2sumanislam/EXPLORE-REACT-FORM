43e0d578e3e53546e83484d9f6157fcea7fc4e6c
me2sumanislam <me2sumanislam247@gmail.com>
2026-03-11 22:22:28 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+ import SimpleForm from './componant/SimpleForm/SimpleForm'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>Explore React Form</h1>
+      <SimpleForm></SimpleForm>
+       
+    </>
+  )
+}
+
+export default App
---
+++ src/componant/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,23 @@
+ import React from 'react'
+ 
+//task-1 submit button click korle nicher function call hobe 
+
+
+
+
+
+ function SimpleForm() {
+   return (
+     <div>
+       <form onSubmit={``}>
+        <input type = "text" />
+        <br />
+        <input type="submit" value="Submit" />
+
+       </form>
+     </div>
+   )
+ }
+ 
+ export default SimpleForm
+ 
